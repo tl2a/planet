d1307b5021fc9bf9005a358b4a4f0ff171898a69
Arpan Biswas <talk2william@protonmail.com>
2019-12-29 13:30:05 +0530

    :full_moon: planet

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
-var frameNumber = 0, // start video at frame 0
-// lower numbers = faster playback
-playbackConst = 6000, 
-// get page height from video duration
-setHeight = document.getElementById("set-height"), 
 // select video element         
-vid = document.getElementById('v0');
-btn = document.querySelector('.tap-on')
+var vid = document.getElementById('v0'),
+btn = document.querySelector('.tap-on');
 btn.addEventListener('mousedown',function() {
   this.classList.add('onhold');
   vid.play();
